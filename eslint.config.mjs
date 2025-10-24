7c1ab72613e4cd04e3ef451999de88578ebfebe5
devlopersabbir <devlopersabbir@gmail.com>
2025-10-24 21:32:13 +0600

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import js from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+    globalIgnores([
+        "node_modules/*",
+        "dist/*",
+        "logs/*",
+        "prisma/*",
+        "generated/*",
+        "scripts.sh",
+        "*.sh",
+        "scripts/*",
+    ]),
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        rules: {
+            "no-useless-catch": "off",
+            "no-console": ["warn", { allow: ["warn", "error", "info", "group", "groupEnd"] }],
+            "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+            "@typescript-eslint/no-explicit-any": "off",
+            "no-unused-expressions": "error",
+        },
+    },
+    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+        rules: {
+            "@typescript-eslint/no-empty-object-type": "warn",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-namespace": "warn",
+            "no-undef": "off",
+        },
+    },
+    eslintPluginPrettierRecommended,
+]);
---
+++ src/@types/app-metadata.ts
@@ -0,0 +1,6 @@
+export interface IAppMetadata {
+    name: string;
+    displayName: string;
+    version: string;
+    description: string;
+}

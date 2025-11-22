d08db044d612b73235146de778a894e6ad148eb9
devlopersabbir <devlopersabbir@gmail.com>
2025-11-22 07:12:23 +0600

    ✨ feat: Integrate S3 for file storage and configure Swagger auth
    This commit introduces S3 integration for file storage, including:
    - Installing necessary packages for S3 functionality.
    - Creating an S3 module and service to handle S3 operations.
    - Configuring Swagger for API documentation with Bearer authentication.
    - Configuring S3 module inside the user module.

--- src/global/s3/s3.module.ts
+++ src/global/s3/s3.module.ts
@@ -1,7 +1,21 @@
-@Module({
-    imports: [],
-    controllers: [],
-    providers: [],
-    exports: [],
-})
-export class S3Module {}
+import { DynamicModule, Module } from "@nestjs/common";
+import { S3ModuleOptions, S3_MODULE_OPTIONS } from "./s3.options";
+import { S3Service } from "./s3.service";
+
+@Module({})
+export class S3Module {
+    static forRoot(options: S3ModuleOptions): DynamicModule {
+        return {
+            module: S3Module,
+            global: true,
+            providers: [
+                {
+                    provide: S3_MODULE_OPTIONS,
+                    useValue: options,
+                },
+                S3Service,
+            ],
+            exports: [S3Service],
+        };
+    }
+}
--- src/main/users/users.module.ts
+++ src/main/users/users.module.ts
@@ -1,10 +1,22 @@
+import { S3Module } from "@global/s3/s3.module";
 import { Module } from "@nestjs/common";
 import { UserRepository } from "./auth.repository";
 import { UsersController } from "./users.controller";
-import { UsersService } from "./users.service";
 
 @Module({
-    providers: [UserRepository, UsersService],
+    imports: [
+        S3Module.forRoot({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+            bucket: process.env.AWS_S3_BUCKET_NAME!,
+            region: process.env.AWS_REGION!,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+            cache: {
+                isCache: true,
+                options: {},
+            },
+        }),
+    ],
+    providers: [UserRepository],
     controllers: [UsersController],
 })
 export class UsersModule {}
